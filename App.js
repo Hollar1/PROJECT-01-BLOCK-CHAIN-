6484f32b34b7859ed2bfaaf778071b4ba602f27f
holla1 <ayenihola@yahoo.com>
2024-05-01 19:50:12 +0100

    firstCommit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View,Image,ImageBackground, TextInput} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex:1,alignItems:"center",justifyContent:"center",backgroundColor:"black"}}>
+      <Text style={{color:"red"}}>
+        ola
+      </Text>
+
+  
+    </View>
+   
+  );
+}
+
