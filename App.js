8316a8f7209e0e96446b37c175568e4002978caa
Prosper <gndptb@gmail.com>
2024-05-04 09:34:53 +0100

    update

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from './src/screens/Home';
 import Details from './src/screens/Details';
